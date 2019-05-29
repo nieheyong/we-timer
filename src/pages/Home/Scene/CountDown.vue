bb084de32e5b30db30009d93b157a43fef1d2c79
Heyong Nie <niehey@163.com>
2019-05-29 23:10:55 +0800

    update

--- src/pages/Home/Scene/CountDown.vue
+++ src/pages/Home/Scene/CountDown.vue
@@ -96,17 +96,10 @@ export default {
       currentCount: 1
     }
   },
-  mounted() {
-    this.startTime = Date.now()
-    this.startCountDown()
-    this.runing = true
-  },
-  beforeDestroy() {
-    this.runing = false
-  },
+
   computed: {
     params() {
-      return { prepareSec: 3, count: 4, workSec: 20, restSec: 10 }
+      return { PREP_SEC: 3, COUNT: 4, WORK_SEC: 20, REST_SEC: 10 }
     },
     prepareTimeStr() {
       return this.remain.prepareSec.toString().padStart(2, 0)
@@ -117,6 +110,14 @@ export default {
     restTimeStr() {
       return secToTimeStr(this.remain.restSec)
     }
+  },
+  mounted() {
+    this.runing = true
+    this.startTime = Date.now()
+    this.startCountDown()
+  },
+  beforeDestroy() {
+    this.runing = false
   },
   methods: {
     togglePause() {
@@ -132,86 +133,84 @@ export default {
     async startCountDown() {
       while (true) {
         await delay(100)
-        if (this.pauseTime || this.finish || !this.runing) {
-          return
-        }
-        this.updateUi()
+        if (this.pauseTime || this.finish || !this.runing) return
+        this.updateCountDown()
       }
     },
-    updateUi() {
-      const { prepareSec, count, workSec, restSec } = this.params
+    updateCountDown() {
+      const { PREP_SEC, COUNT, WORK_SEC, REST_SEC } = this.params
+      const remain = this.remain
       const expireMs = Date.now() - this.startTime
       const extraMs = expireMs % 1000
       const expireSec = Math.floor(expireMs / 1000)
-      if (expireSec < prepareSec) {
+      if (expireSec < PREP_SEC) {
         this.isPrepare = true
-        const remainPrepareSec = prepareSec - expireSec
+        const remainPrepareSec = PREP_SEC - expireSec
         this.percent =
-          100 -
-          ((remainPrepareSec * 1000 - extraMs) / (prepareSec * 1000)) * 100
+          100 - ((remainPrepareSec * 1000 - extraMs) / (PREP_SEC * 1000)) * 100
         if ([3, 2, 1].includes(remainPrepareSec)) {
           this.warn('prepare', remainPrepareSec, 'ding.mp3')
         }
-        this.remain.prepareSec = remainPrepareSec
+        remain.prepareSec = remainPrepareSec
       } else {
-        this.remain.prepareSec = 0
-        const startSec = expireSec - prepareSec
+        remain.prepareSec = 0
+        const startSec = expireSec - PREP_SEC
         this.isPrepare = false
-        const ONE_COUNT_SEC = workSec + restSec
+        const ONE_COUNT_SEC = WORK_SEC + REST_SEC
         this.currentCount = Math.floor(startSec / ONE_COUNT_SEC) + 1
 
-        const temp1 = startSec % ONE_COUNT_SEC
-        if (temp1 === 0) {
+        const tempSec = startSec % ONE_COUNT_SEC
+        if (tempSec === 0) {
           this.warn('start', this.currentCount, 'dong.mp3')
         }
-        if (temp1 < workSec) {
-          this.remain.restSec = 0
-          this.remain.workSec = workSec - temp1
+        if (tempSec < WORK_SEC) {
+          remain.restSec = 0
+          remain.workSec = WORK_SEC - tempSec
           this.percent =
-            ((this.remain.workSec * 1000 - extraMs) / (workSec * 1000)) * 100
+            ((remain.workSec * 1000 - extraMs) / (WORK_SEC * 1000)) * 100
           this.isRest = false
           this.isWork = true
         } else {
-          if (this.currentCount === count) {
-            this.remain.workSec = 0
+          if (this.currentCount === COUNT) {
+            remain.workSec = 0
             this.percent = 0
             this.finish = true
             this.playVoice('success.mp3')
             this.$emit('finish')
             return
           }
-          this.remain.workSec = 0
+          remain.workSec = 0
           this.warn('rest', this.currentCount, 'rest.mp3')
-          const remainRestSec = restSec - (temp1 - workSec)
+          const remainRestSec = REST_SEC - (tempSec - WORK_SEC)
           this.percent =
-            100 - ((remainRestSec * 1000 - extraMs) / (restSec * 1000)) * 100
+            100 - ((remainRestSec * 1000 - extraMs) / (REST_SEC * 1000)) * 100
           this.isRest = true
           this.isWork = false
           if ([3, 2, 1].includes(remainRestSec)) {
             this.warn(this.currentCount, remainRestSec, 'ding.mp3')
           }
-          this.remain.restSec = remainRestSec
+          remain.restSec = remainRestSec
         }
       }
     },
     comfirm() {
       this.$store.commit('slideToScene', SCENE.Start)
     },
+    warn(prefix, id, fileName) {
+      const key = `${prefix}-${id}`
+      if (!this.warnMap) {
+        this.warnMap = {}
+      }
+      if (!this.warnMap[key]) {
+        this.warnMap[key] = true
+        this.playVoice(fileName)
+      }
+    },
     playVoice(name) {
       const audio = uni.createInnerAudioContext()
       audio.src = '/static/audio/' + name
       audio.play()
       audio.onEnded(audio.destroy)
-    },
-    warn(prefix, id, fileName) {
-      const key = `${prefix}-${id}`
-      if (!this.vibrateMap) {
-        this.vibrateMap = {}
-      }
-      if (!this.vibrateMap[key]) {
-        this.vibrateMap[key] = true
-        this.playVoice(fileName)
-      }
     }
   }
 }
